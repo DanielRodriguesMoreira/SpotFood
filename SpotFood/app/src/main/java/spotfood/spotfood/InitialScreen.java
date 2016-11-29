04f56410d447d9356448b99985f3847d7b4cad08
DoonPsdn <tiagojosecs@hotmail.com>
2016-11-29 10:27:26 +0000

    asdf

---
+++ SpotFood/app/src/main/java/spotfood/spotfood/InitialScreen.java
@@ -0,0 +1,13 @@
+package spotfood.spotfood;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class InitialScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_initial_screen);
+    }
+}
