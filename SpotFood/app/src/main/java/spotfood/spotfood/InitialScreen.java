1fc682ecb20302e80b2e5a707e2cab4129f04012
drmoreira <a21240321@alunos.isec.pt>
2016-12-01 18:38:02 +0000

    coding standars

--- SpotFood/app/src/main/java/spotfood/spotfood/InitialScreen.java
+++ SpotFood/app/src/main/java/spotfood/spotfood/InitialScreen.java
@@ -1,3 +1,14 @@
+/*
+ * SpotFood - 2016
+ *
+ * Authors:
+ *          -> Daniel Moreira nº21240321
+ *          -> Hugo Santos nº21220702
+ *          -> Tiago Santos nº21230530
+ *          -> Carlos Zambrano nº 21260582
+ *          -> Selman Ay nº21260599
+ */
+
 package spotfood.spotfood;
 
 import android.app.Activity;
@@ -15,25 +26,23 @@ import android.widget.ArrayAdapter;
 import android.widget.ImageButton;
 import android.widget.ListView;
 import android.widget.TextView;
-
 import com.example.poili.spotfood.R;
 import com.google.firebase.database.DataSnapshot;
 import com.google.firebase.database.DatabaseError;
 import com.google.firebase.database.DatabaseReference;
 import com.google.firebase.database.FirebaseDatabase;
 import com.google.firebase.database.ValueEventListener;
-
 import java.util.ArrayList;
 import java.util.Calendar;
 
 public class InitialScreen extends Activity {
 
-    private DatabaseReference SpotFood;
-    ImageButton searchButton;
-    TextView searchText;
-    ListView listRestaurants;
-    ArrayAdapter<String> adapter;
-    public ArrayList<String> restaurant;
+    private DatabaseReference mSpotFoodDataBaseReference;
+    private ImageButton mSearchButton;
+    private TextView mSearchText;
+    private ListView mListRestaurants;
+    private ArrayAdapter<String> mAdapter;
+    private ArrayList<String> mRestaurantsList;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -41,71 +50,53 @@ public class InitialScreen extends Activity {
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);      //Teste para tirar a barra do titulo
         setContentView(R.layout.activity_initial_screen);
 
+        //check internet Connection
         if(!hasNetworkConnection()){
             internetConnectionErrorDialog md = new internetConnectionErrorDialog();
             md.show(getFragmentManager(),"TAG");
         }
 
-        SpotFood = FirebaseDatabase.getInstance().getReference();
+        //Get database reference
+        mSpotFoodDataBaseReference = FirebaseDatabase.getInstance().getReference();
 
-        restaurant = new ArrayList<>();
-        this.searchButton = (ImageButton) findViewById(R.id.searchButton);
-        this.listRestaurants = (ListView) findViewById(R.id.listRestaurants);
-        this.searchText = (TextView) findViewById(R.id.searchText);
+        //inicialize variables
+        this.inicializeVariables();
 
+        //show open restaurants
+        this.searchOpenRestaurants();
+    }
 
-        this.searchButton.setOnClickListener(new View.OnClickListener() {
+    /** Inicialize all the variables */
+    private void inicializeVariables() {
+        this.mRestaurantsList = new ArrayList<>();
+        this.mSearchButton = (ImageButton) findViewById(R.id.searchButton);
+        this.mListRestaurants = (ListView) findViewById(R.id.listRestaurants);
+        this.mSearchText = (TextView) findViewById(R.id.searchText);
+        this.mSearchButton.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                if (searchText.getText().toString() == null || searchText.getText().length() == 0) {
-                    searchOpenRestaurant();
-                    adapter.notifyDataSetChanged();
+                if (mSearchText.getText().toString() == null || mSearchText.getText().length() == 0) {
+                    searchOpenRestaurants();
                 }
                 else {
-                    searchRestaurantByNameOrTypeOfFood(searchText.getText().toString());
+                    searchRestaurantByNameOrTypeOfFood(mSearchText.getText().toString());
                 }
-                /*
-                createRestaurant(new Restaurant("3", "0", "Casa dos bifes", new Hour(new Date(), new Date()),
-                        new Hour(new Date(), new Date()),
-                        new Hour(new Date(), new Date()),
-                        new Hour(new Date(), new Date()),
-                        new Hour(new Date(), new Date()),
-                        new Hour(new Date(), new Date()),
-                        new Hour(new Date(), new Date()),   "231568123", "Rua principal", new ArrayList<String>()));
-
-                createRestaurant(new Restaurant("4", "0", "Rei das Bifanas", new Hour(new Date(), new Date()),
-                    new Hour(new Date(), new Date()),
-                    new Hour(new Date(), new Date()),
-                    new Hour(new Date(), new Date()),
-                    new Hour(new Date(), new Date()),
-                    new Hour(new Date(), new Date()),
-                    new Hour(new Date(), new Date()),   "231568123", "Rua principal", new ArrayList<String>()));
-
-                ArrayList<String> tipoDeComida = new ArrayList<String>();
-                tipoDeComida.add(new String("Bifanas"));
-                createRestaurant(new Restaurant("5", "0", "Comes de tudo", new Hour(new Date(), new Date()),
-                    new Hour(new Date(), new Date()),
-                    new Hour(new Date(), new Date()),
-                    new Hour(new Date(), new Date()),
-                    new Hour(new Date(), new Date()),
-                    new Hour(new Date(), new Date()),
-                    new Hour(new Date(), new Date()),   "231568123", "Rua principal", tipoDeComida));
-                    */
             }
 
         });
-        adapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, restaurant);
-        this.listRestaurants.setAdapter(adapter);
+
+        //Set mAdapter to one row of list view
+        this.mAdapter = new ArrayAdapter<String>(this, android.R.layout.simple_list_item_1, mRestaurantsList);
+        this.mListRestaurants.setAdapter(this.mAdapter);
 
     }
-    private void createRestaurant(Restaurant r) {
-        SpotFood.child("restaurants").child(r.getIdRestaurant()).setValue(r);
-    }
 
+    /** Search on firebase database all the restaurants that are open at the current time */
+    private void searchOpenRestaurants() {
 
+        mRestaurantsList.clear();
 
-    private void searchOpenRestaurant(){
-        DatabaseReference userRef = this.SpotFood.child("restaurants");
+        DatabaseReference userRef = this.mSpotFoodDataBaseReference.child("restaurants");
         userRef.addListenerForSingleValueEvent(new ValueEventListener() {
             Calendar c = Calendar.getInstance();
             int currentTime = c.get(Calendar.HOUR_OF_DAY)*100+c.get(Calendar.MINUTE);
@@ -118,27 +109,35 @@ public class InitialScreen extends Activity {
                     Restaurant u = postSnapshot.getValue(Restaurant.class);
                     restaurantOpen = u.getRestaurantHour(day).getOpen();
                     restaurantClose = u.getRestaurantHour(day).getClose();
-                    System.out.println("Restaurant:"+u.getName()+" open:"+restaurantOpen+" close:"+restaurantClose + " current:"+currentTime);
-                    if(restaurantOpen < restaurantClose && (currentTime >= u.getRestaurantHour(day).getOpen() && currentTime < u.getRestaurantHour(day).getClose()))
-                        restaurant.add(u.getName());
-                    else if( restaurantOpen > restaurantClose && (currentTime >= u.getRestaurantHour(day).getOpen() || currentTime < u.getRestaurantHour(day).getClose()))
-                        restaurant.add(u.getName());
+
+                    //in case open hours are lower than close hours and the current time is in that range
+                    if(restaurantOpen < restaurantClose && (currentTime >= u.getRestaurantHour(day).getOpen()
+                            && currentTime < u.getRestaurantHour(day).getClose())) {
+                        mRestaurantsList.add(u.getName());
+                    }
+                    /* in case open hours are higher than close hours and the current time is higher
+                     than open hours and lower than close hours*/
+                    else if( restaurantOpen > restaurantClose && (currentTime >= u.getRestaurantHour(day).getOpen()
+                            || currentTime < u.getRestaurantHour(day).getClose())) {
+                        mRestaurantsList.add(u.getName());
+                    }
                 }
+
+                mAdapter.notifyDataSetChanged();
             }
 
             @Override
-            public void onCancelled(DatabaseError databaseError) {
-
-            }
+            public void onCancelled(DatabaseError databaseError) { }
         });
     }
 
-    private void searchRestaurant() {
+    /** Search on firebase database all the restaurants (this is for administrator) */
+    private void searchAllRestaurants() {
 
-        restaurant.clear();
+        mRestaurantsList.clear();
 
         //Get restaurants reference
-        final DatabaseReference restaurantsRef = this.SpotFood.child("restaurants");
+        final DatabaseReference restaurantsRef = this.mSpotFoodDataBaseReference.child("restaurants");
 
         //add Listener
         restaurantsRef.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -149,12 +148,11 @@ public class InitialScreen extends Activity {
                 //Cicle For that go through all the restaurants in firebase database
                 for (DataSnapshot postSnapshot: dataSnapshot.getChildren()) {
                     Restaurant rest = postSnapshot.getValue(Restaurant.class);
-                    restaurant.add(rest.getName());
-                    searchText.setText(searchText.getText() + ", " + rest.getName());
+                    mRestaurantsList.add(rest.getName());
                 }
 
                 //refresh ListView
-                adapter.notifyDataSetChanged();
+                mAdapter.notifyDataSetChanged();
             }
 
             @Override
@@ -162,19 +160,20 @@ public class InitialScreen extends Activity {
         });
     }
 
+    /** Search on firebase database all the restaurants by their name or type of food */
     private void searchRestaurantByNameOrTypeOfFood(final String nameOrTypeOfFood) {
 
         //Check if string is null or empty
         if (nameOrTypeOfFood == null || nameOrTypeOfFood.isEmpty()) {
-            searchRestaurant();
+            searchAllRestaurants();
             return;
         }
 
         //clear ListView
-        restaurant.clear();
+        mRestaurantsList.clear();
 
         //Get restaurants reference
-        final DatabaseReference restaurantsRef = this.SpotFood.child("restaurants");
+        final DatabaseReference restaurantsRef = this.mSpotFoodDataBaseReference.child("restaurants");
 
         //add Listener
         restaurantsRef.addListenerForSingleValueEvent(new ValueEventListener() {
@@ -192,7 +191,7 @@ public class InitialScreen extends Activity {
                             //if (typeOfFood.toUpperCase().equals(nameOrTypeOfFood.toUpperCase())) {
                             if(typeOfFood.toUpperCase().contains(nameOrTypeOfFood.toUpperCase())){
                                 find = true;
-                                restaurant.add(rest.getName());
+                                mRestaurantsList.add(rest.getName());
                                 break;
                             }
                         }
@@ -201,13 +200,13 @@ public class InitialScreen extends Activity {
                     //search by name if not find by type of food
                     if(!find){
                         if(rest.getName().toUpperCase().contains(nameOrTypeOfFood.toUpperCase())){
-                            restaurant.add(rest.getName());
+                            mRestaurantsList.add(rest.getName());
                         }
                     }
                 }
 
                 //refresh ListView
-                adapter.notifyDataSetChanged();
+                mAdapter.notifyDataSetChanged();
             }
 
             @Override
@@ -215,6 +214,7 @@ public class InitialScreen extends Activity {
         });
     }
 
+    /** Check if there is internet connection on android phone */
     private boolean hasNetworkConnection(){
         ConnectivityManager cm = (ConnectivityManager)this.getSystemService(Context.CONNECTIVITY_SERVICE);
 
@@ -223,6 +223,7 @@ public class InitialScreen extends Activity {
 
     }
 
+    /** Fragment Dialog to show internet connection error */
     class internetConnectionErrorDialog extends DialogFragment {
         @Override
         public Dialog onCreateDialog(Bundle savedInstanceState) {
