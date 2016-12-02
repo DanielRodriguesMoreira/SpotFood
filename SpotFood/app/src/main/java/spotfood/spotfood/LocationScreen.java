6b10cecf6216c316e3f967a7bcc4810a10da9eb2
drmoreira <a21240321@alunos.isec.pt>
2016-12-02 10:17:03 +0000

    add LocationScreen.java and LoginScreen.java

---
+++ SpotFood/app/src/main/java/spotfood/spotfood/LocationScreen.java
@@ -0,0 +1,28 @@
+/*
+ * SpotFood - 2016
+ *
+ * Authors:
+ *          -> Daniel Moreira nº21240321
+ *          -> Hugo Santos nº21220702
+ *          -> Tiago Santos nº21230530
+ *          -> Carlos Zambrano nº 21260582
+ *          -> Selman Ay nº21260599
+ */
+
+package spotfood.spotfood;
+
+import android.app.Activity;
+import android.os.Bundle;
+import android.view.Window;
+
+import com.example.poili.spotfood.R;
+
+public class LocationScreen extends Activity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        this.requestWindowFeature(Window.FEATURE_NO_TITLE);
+        setContentView(R.layout.location);
+    }
+}
