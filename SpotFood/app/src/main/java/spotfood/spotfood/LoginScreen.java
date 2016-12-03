c6bd3d18dda20209627c63e1902f7747ff0bd3df
drmoreira <a21240321@alunos.isec.pt>
2016-12-03 23:19:39 +0000

    createAccount almost finished

--- SpotFood/app/src/main/java/spotfood/spotfood/LoginScreen.java
+++ SpotFood/app/src/main/java/spotfood/spotfood/LoginScreen.java
@@ -12,17 +12,35 @@
 package spotfood.spotfood;
 
 import android.app.Activity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
 import android.view.Window;
+import android.widget.Button;
 
 import com.example.poili.spotfood.R;
 
 public class LoginScreen extends Activity {
+
+    private Button mCreateAccountButton;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         this.requestWindowFeature(Window.FEATURE_NO_TITLE);
         setContentView(R.layout.login);
+
+        this.inicializeVariables();
+    }
+
+    private void inicializeVariables() {
+        this.mCreateAccountButton = (Button)findViewById(R.id.newAccount);
+        this.mCreateAccountButton.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                Intent intent = new Intent(getApplication(), CreateAccountScreen.class);
+                startActivity(intent);
+            }
+        });
     }
 }
